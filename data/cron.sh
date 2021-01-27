9b217dbced75aa8a9ea74024ead1c0a5b6c2686a
princelySid <sidney.ochieng@gmail.com>
2021-01-27 16:14:55 +0300

    cron script

---
+++ data/cron.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+VENV_PYTHON="/path/to/virtual/environment/bin/activate"
+PROJECT="/path/to/yt-bot"
+PY_FILE="path/to/tweet_videos.py"
+LOG_FILE="path/to/log/tweet.log"
+
+source "$VENV_PYTHON" && cd "$PROJECT" && python "$PY_FILE" >> "$LOG_FILE" 2>&1
