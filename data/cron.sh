b280e39a53b6cddc711de0ef28f417020188a968
princelySid <sidney.ochieng@gmail.com>
2021-01-27 17:01:24 +0300

    small change

--- data/cron.sh
+++ data/cron.sh
@@ -2,7 +2,7 @@
 
 VENV_PYTHON="/path/to/virtual/environment/bin/activate"
 PROJECT="/path/to/yt-bot"
-PY_FILE="path/to/tweet_videos.py"
+PY_FILE="data/tweet_videos.py"
 LOG_FILE="path/to/log/tweet.log"
 
 source "$VENV_PYTHON" && cd "$PROJECT" && python "$PY_FILE" >> "$LOG_FILE" 2>&1
